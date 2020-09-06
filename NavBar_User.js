43f0f088d001050dffa5e7a7ce756b173eb53cbf
cesargafer <cesargafer@users.noreply.github.com>
2020-09-06 02:12:50 -0500

    Add ID-001
    Add ID-001
    
    Sprint backlog # 1
    Product backlog: Estrategias de mejora
    Actividad integradora. Sprint, el corazón de SCRUM

---
+++ NavBar_User.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Nav from 'react-bootstrap/Nav';
+
+function NavBar_User() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Nav
+        activeKey="/home"
+        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+        >
+        <Nav.Item>
+          <Nav.Link href="/home">Consultar información</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="Descargar">Descargar reportes</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="Configurar">Configurar lineas de producción</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>
+            Administrador del sitio
+          </Nav.Link>
+        </Nav.Item>
+        </Nav>
+      </header>
+    </div>
+  );
+}
+
+export default NavBar_User;
