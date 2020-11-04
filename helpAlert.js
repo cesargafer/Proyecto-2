03e80a08ec1bc6db994c928c3135e21d233ef3ff
cesargafer <cesargafer@users.noreply.github.com>
2020-11-03 23:20:29 -0600

    Add Sprint backlog # 2 ID-009
    Ayuda
    
    Desempeñando el rol de usuario necesito contar con la facilidad de navegación y acceso a canales de ayuda con la finalidad de tener una mejor experiencia de usuario.

---
+++ helpAlert.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Alert } from 'reactstrap';
+
+// Sprint backlog # 2 ID-009
+
+const HelpAlert = (props) => {
+  return (
+    <div>
+      <Alert color="primary">
+        Ayuda :D
+      </Alert>
+     </div>
+  );
+};
+
+export default HelpAlert;
